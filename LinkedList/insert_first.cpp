7281011c7c9dce97c34712d498884bc1bbfcf08f
Tawhidur Rahman <tawhidurrahmanhimuhimu@gmail.com>
2026-01-28 16:36:55 +0600

    insert first

---
+++ LinkedList/insert_first.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+class node{
+    public:
+    int data;
+    node*next;
+};
+
+int main() {
+	// your code goes here
+	int n,x;
+	cin>>n;
+	node*head=NULL;
+	node*tail=NULL;
+	for(int i=0;i<n;i++){
+	    cin>>x;
+	    node*newnode=new node();
+	    newnode->data=x;
+	    newnode->next=NULL;
+	    if(head==NULL)
+	    head=tail=newnode;
+	    else{
+	        tail->next=newnode;
+	        tail=newnode;
+	    }
+	}
+	int val;
+	cin>>val;
+	node*newnode=new node();
+	newnode->data=val;
+	newnode->next=NULL;
+	if(head==NULL)
+	head=tail=newnode;
+	else{
+	    newnode->next=head;
+	    head=newnode;
+	}
+	node*temp=head;
+	while(temp!=NULL){
+	    cout<<temp->data<<" ";
+	    temp=temp->next;
+	}
+
+}
