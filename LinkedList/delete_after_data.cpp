8506fffd21ad9549a2ba35cf304e32a92a20316c
Tawhidur Rahman <tawhidurrahmanhimuhimu@gmail.com>
2026-04-01 09:01:50 -0900

    delete after data

---
+++ LinkedList/delete_after_data.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class node {
+public:
+    int data;
+    node* next;
+};
+
+node* createList(int n) {
+    node* head = NULL;
+    node* tail = NULL;
+    int x;
+    for (int i = 0; i < n; i++) {
+        cin >> x;
+        node* newnode = new node();
+        newnode->data = x;
+        newnode->next = NULL;
+        if (!head) head = tail = newnode;
+        else { tail->next = newnode; tail = newnode; }
+    }
+    return head;
+}
+
+node* deleteAfterData(node* head, int target) {
+    node* temp = head;
+    while (temp && temp->data != target) temp = temp->next;
+    if (temp && temp->next) {
+        node* del = temp->next;
+        temp->next = del->next;
+        delete del;
+    }
+    return head;
+}
+
+void printList(node* head) {
+    while (head) { cout << head->data << " "; head = head->next; }
+    cout << endl;
+}
+
+int main() {
+    int n, key;
+    cin >> n;
+    node* head = createList(n);
+
+    cin >> key; // delete after this value
+    head = deleteAfterData(head, key);
+
+    printList(head);
+}
